0a16dab34f6352a19350a887d14f401e88f80519
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-04-11 08:49:45 -0500

    Add files via upload

---
+++ StudentClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assigment1._4.StudentClass;
+
+namespace Assigment1._4
+{
+internal class StudentClass
+{
+    internal class Student
+    {
+        private int studentId;
+        private string studentFname;
+        private string studentLname;
+        private char studentGrade;
+
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+        public string StudentFname
+            {
+            get { return studentFname; }
+            set { studentFname = value; }
+        }
+        public string StudentLname
+            {
+                get { return studentLname; }
+                set { studentLname = value; }
+            }
+        public char StudentGrade
+            {
+                get { return studentGrade; }
+                set { studentGrade = value; }
+            }
+    }
+
+
+}
+}
+
