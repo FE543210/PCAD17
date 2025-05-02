a9d45d68649bf2cbcdda21cd1e55e150a55c5731
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-05-02 09:01:59 -0500

    Missing 2 and 3 Added

---
+++ Assignments4.3/Frequency.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4._3_Version2
+{
+    internal class Frequency
+    {
+        public static void howMany()
+        {
+            //Clear methods with 1 purpose and less then 10 lines?
+            Console.WriteLine("Input the number of elements");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numberArray = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"Input element{i}: ");
+                numberArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Dictionary<int, int> frequencyCounter = new Dictionary<int, int>();
+            
+            foreach (int i in numberArray) {
+                if (frequencyCounter.ContainsKey(i))
+                    frequencyCounter[i]++;
+                else
+                {
+                    frequencyCounter[i] = 1;
+                }
+            }
+            Console.WriteLine("Frequency of all elements of array :");
+            foreach (var i in frequencyCounter)
+            {
+                Console.WriteLine($"{i.Key} ocurrs {i.Value} times");
+            }
+
+
+
+        }
+    }
+}
---
+++ Assignments4.3/UniqueNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4._3_Version2
+{
+    internal class UniqueNumbers
+    {
+        public static void unique()
+        {
+            //Clear methods with 1 purpose and less then 10 lines?
+            Console.WriteLine("Input the number of elements");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numberArray = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"Input element{i}: ");
+                numberArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Dictionary<int, int> frequencyCounter = new Dictionary<int, int>();
+
+            foreach (int i in numberArray)
+            {
+                if (frequencyCounter.ContainsKey(i))
+                    frequencyCounter[i]++;
+                else
+                {
+                    frequencyCounter[i] = 1;
+                }
+            }
+            Console.WriteLine("The unique elements found in the array are:");
+            foreach (var i in frequencyCounter)
+            {
+                if(i.Value == 1)
+                {
+                    Console.WriteLine(i.Key);
+                }
+            }
+
+
+
+        }
+    }
+}
