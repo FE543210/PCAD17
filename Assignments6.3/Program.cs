5a4d00cefae2a9e835948986abcc1b38bec24545
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-05-15 14:14:30 -0700

    Add files via upload

---
+++ Assignments6.3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignments6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Q myq = new();
+            myq.Enqueu("111-457-4715");
+            myq.Enqueu("222-457-4715");
+            myq.Enqueu("333-457-4715");
+            myq.Display();
+            myq.Deque();
+            myq.Deque();
+            myq.Display();
+            myq.Deque();
+            myq.Display();
+
+        }
+    }
+}
---
+++ Assignments6.3/phonebook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments6._3
+{
+    class PhoneNode
+    {
+        public string phoneNumber; //Using string since sometimes user can input -,(,).
+        public PhoneNode next;
+        public PhoneNode(string val, PhoneNode next) {
+            this.phoneNumber = val;
+            this.next = next;
+        }
+    }
+    class Q
+    {
+        PhoneNode front;//Head
+        PhoneNode rear; //tail
+        int size;
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+        public void Enqueu(string val)//AddLast() O(1)
+        {
+            PhoneNode newNode = new PhoneNode(val, null);
+            if (IsEmpty())
+            {
+                front = newNode;
+            }
+            else
+            {
+                rear.next = newNode;
+            }
+            rear = newNode;
+            size++;
+
+        }
+        public string? Deque()
+        {   
+            if (IsEmpty())
+            {
+                Console.WriteLine("Nothing to Deque");
+                return null;
+            }
+            string val = front.phoneNumber;
+            front = front.next;
+            size--;
+            if (IsEmpty())
+            {
+                rear = null;
+            }
+            return val;
+        }
+        public void Display()
+        {
+            if (IsEmpty()) { Console.WriteLine("Nothing to Display!!"); return; }
+            PhoneNode temp = front;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.phoneNumber + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("--------------");
+        }
+    }
+}
