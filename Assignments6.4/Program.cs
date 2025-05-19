56ad0dc53050dc0f2c7be874e07a5ed457d92fb7
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-05-19 07:06:58 -0700

    Add files via upload

--- Assignments6.4/Program.cs
+++ Assignments6.4/Program.cs
@@ -1,43 +1,60 @@
-﻿namespace Assignments6._4
+﻿using System.Numerics;
+
+namespace Assignments6._4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[][] testOne = new int[][]
-            {
-            new int[] {1, 2, 3},
-            new int[] {4, 5, 6},
-            new int[] {7, 8, 9}
-            };
-            int[][] testTwo = new int[][]
-            {
-            new int[] {5, 1, 9,11},
-            new int[] {2, 4, 8,10},
-            new int[] {13, 3, 6,7},
-            new int[] {15,14,12,16}
-            };
+
+            int[,] testOne = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             Rotate(testOne);
-            Console.WriteLine();
-            Rotate(testTwo);
-        }
-        //Leetcode #48
-        public static void Rotate(int[][] matrix)
-        {
-            ///Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-            //Output: [[7, 4, 1],[8, 5, 2],[9, 6, 3]]
-            int temp = 0;
-            for (int j = 0; j <= matrix.Length - 1; j++)
+            for(int i = 0; i< testOne.GetLength(0); i++)
             {
-                for (int i = matrix.Length - 1; i >= 0; i--)
+                for(int x = 0; x < testOne.GetLength(0); x++)
                 {
-                    Console.Write(matrix[i][j] + " ");
+                    Console.Write(testOne[i,x] + " ");
+
                 }
                 Console.WriteLine();
             }
-
-
         }
-        
+
+        public static void Rotate(int[,] matrix)
+        {
+            //    ///Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+            //    //Output: [[7, 4, 1],[8, 5, 2],[9, 6, 3]]
+            int matrixRow = matrix.GetLength(0);
+            if (matrixRow < 1) { return; }
+
+            for(int i=0; i<matrixRow; i++)
+            {
+                for(int x = i +1; x < matrixRow; x++)
+                {
+                    int temp = matrix[i, x];
+                    matrix[i, x] = matrix[x, i];
+                    matrix[x, i] = temp;
+                    //3 Way switch
+                    //[1,4,7]
+                    //[2,5,8]
+                    //[3,6,9]
+                }
+            }
+            for(int i = 0; i <matrixRow; i++)
+            {
+                for (int x = 0; x < matrixRow/2; x++)
+                {
+                    int temp = matrix[i, x];
+                    matrix[i, x] = matrix[i, matrixRow - x - 1];
+                    matrix[i, matrixRow - x - 1] = temp;
+                }
+                
+            }
+            //[7, 4, 1]
+            //[8, 5, 2]
+            //[9, 6, 3]]
+            //No need to return, matrix is reff type
+        }
+
     }
 }
