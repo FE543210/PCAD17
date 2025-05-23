01a8f4834927daff7696b8500314a17f615d1c81
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-05-23 07:46:05 -0700

    Add files via upload

---
+++ Assignments7.3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignments7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BST tree = new BST();
+            tree.InsertNode(tree.root, 30);
+            tree.InsertNode(tree.root, 20);
+            tree.InsertNode(tree.root, 45);
+            tree.InsertNode(tree.root, 20);
+            tree.InsertNode(tree.root, 10);
+            tree.Inorder(tree.root);
+
+
+            Node testOne = tree.Search(tree.root, 10);
+            Console.WriteLine(tree.Search(tree.root, 9));//Returns null so ""
+            Console.WriteLine(testOne.data);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignments7.3/Tree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments7._3
+{
+    class Node
+    {
+        public int data;
+        public Node rightPointer;
+        public Node leftPointer;
+
+        public Node(int data, Node right, Node left)
+        {
+            this.data = data;
+            this.rightPointer = right;
+            this.leftPointer = left;
+        }
+    }
+    class BST
+    {
+        public Node root;
+        public BST()
+        {
+            root = null;
+        }
+        public void InsertNode(Node temptRoot, int val)
+        {
+            Node newNode = new Node(val, null, null);
+            Node temp = null;
+            if(root != null)
+            {
+                while (temptRoot!= null)
+                {
+                    temp = temptRoot;
+                    if(temptRoot.data == val)
+                    {
+                        return;
+                    }else if(val < temptRoot.data)
+                    {
+                        temptRoot = temptRoot.leftPointer;
+                    }
+                    else if(val >temptRoot.data)
+                    {
+                        temptRoot = temptRoot.rightPointer;
+                    }
+                   
+                }
+                if (val < temp.data)
+                {
+                    temp.leftPointer = newNode;
+                }
+                else if(val > temp.data)
+                {
+                    temp.rightPointer = newNode;
+                }
+            }
+            else
+            {
+                root = newNode;
+            }
+        }
+        public void Inorder(Node temptroot)
+        {
+            if(temptroot!= null)
+            {
+                Inorder(temptroot.leftPointer);
+                Console.WriteLine(temptroot.data);
+                Inorder(temptroot.rightPointer);
+            }
+        }
+        public Node Search(Node temptRoot, int val)
+        {
+            while (temptRoot != null)
+            {
+
+                if (temptRoot.data == val)
+                {
+                    return temptRoot;
+                }
+                else if (val < temptRoot.data)
+                {
+                    temptRoot = temptRoot.leftPointer;
+                }
+                else if (val > temptRoot.data)
+                {
+                    temptRoot = temptRoot.rightPointer;
+                }
+            }
+            return null;
+        }
+    }
+}
