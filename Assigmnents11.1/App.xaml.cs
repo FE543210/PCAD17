151f2ae6237737bb521054c314a1468432e9bb85
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-06-19 16:20:58 -0500

    Add files via upload

---
+++ Assigmnents11.1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Books
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider _serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddDbContext<Data.BookContext>(options =>
+            {
+                options.UseSqlite("Data Source=books.db");
+            });
+            services.AddSingleton<MainWindow>();
+            _serviceProvider = services.BuildServiceProvider();
+        }
+        private void OnStartUp(Object sender,StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ Assigmnents11.1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Books.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Non depency injection
+        //BookContext dbcontext = new BookContext(new DbContextOptions<BookContext>());
+        BookContext context;
+        Book newbook = new Book();
+        //depend injection
+        public MainWindow(BookContext bookcontext)
+        {
+            InitializeComponent();
+            context = bookcontext;
+            GetBooks();
+            AddProductGrid.DataContext = newbook;
+        }
+        private void GetBooks()
+        {
+            BookDG.ItemsSource = context.Books.ToList();
+        }
+        Book selectedBook;
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    GetBooks();
+        //}
+        private void UpdateBookForEdit(object sender, RoutedEventArgs e)
+        {
+            selectedBook = (sender as FrameworkElement).DataContext as Book;
+            UpdateBookGrid.DataContext = selectedBook;
+        }
+        private void DeleteBookForEdit(object sender, RoutedEventArgs e)
+        {
+            var book = (sender as FrameworkElement).DataContext as Book;
+            if(book != null)
+            {
+                context.Books.Remove(book);
+                context.SaveChanges();
+                GetBooks();
+                MessageBox.Show("Book Removed!");
+            }
+            else
+            {
+                MessageBox.Show("Book Not Found");
+            }
+
+        }
+        private void AddBook(object sender, RoutedEventArgs e)
+        {
+            context.Books.Add(newbook);
+            context.SaveChanges();
+            GetBooks();
+            MessageBox.Show("Book Added Succefully!");
+        }
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            context.Update(selectedBook);
+            context.SaveChanges();
+            GetBooks();
+            MessageBox.Show("Book Updated!");
+        }
+
+    }
+}
