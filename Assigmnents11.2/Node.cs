0d378d1f7436242444962b5cc0efc01ef8c722d6
FE543210 <filipe.candidosilvadosreis@outlook.com>
2025-06-19 16:23:43 -0500

    Add files via upload

---
+++ Assigmnents11.2/Node.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment11._2
+{
+    class Node
+    {
+        public int number { get; set; }//data
+        public Node next;
+        
+        public Node(int num, Node next)
+        {
+            this.number = num;
+            this.next = next;
+        }
+    }
+    class LList
+    {
+        private Node head;
+        private Node tail;
+        private int size;//Devs choice
+        public int Size
+        {
+            get { return this.size; }
+        }
+        public LList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+
+        }
+        public void AddFirst(int num)
+        {
+            Node newNode = new Node(num, null);
+            if (this.IsEmpty())
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.next = this.head;
+                this.head = newNode;
+            }
+            size++;
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (IsEmpty()) { Console.WriteLine("List is empty!!"); }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.number);
+                    temp = temp.next;
+                }
+            }
+        }
+        public void RemoveFirst()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty!!");
+            }
+            else
+            {
+                head = head.next;
+                this.size--;
+                if (IsEmpty()) { tail = null; }
+            }
+        }
+        public void Search(int val)
+        {
+            Node temp = head;
+            int i = 1;
+            while (temp != null)
+            {
+                if (temp.number == val)
+                {
+                    Console.WriteLine($"Number found at index {i}");
+                    return;
+                }
+                temp = temp.next;
+                i++;
+            }
+            Console.WriteLine("Number Not Found");
+        }
+        public void Reverse()
+        {
+            Node prev = null;
+            Node current = this.head;
+            Node next = null;
+            this.tail = head;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            this.head = prev;
+        }
+
+    }
+}
---
+++ Assigmnents11.2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Assigment11._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test1 = [7, 1, 5, 3, 6, 4];//5
+            Console.WriteLine(MaxProfit(test1));
+            Console.WriteLine("------------------");
+            LList test2 = new();
+            test2.AddFirst(5);
+            test2.AddFirst(4);
+            test2.AddFirst(3);
+            test2.AddFirst(2);
+            test2.AddFirst(1);
+            test2.Display();
+            Console.WriteLine("------------------");
+            test2.Reverse();
+            test2.Display();
+        }
+        //LeetCode #21
+
+        public static int MaxProfit(int[] prices)//O(n)
+        {
+            if (prices == null || prices.Length < 2)
+            {
+                return 0;
+            }
+
+            int maxProfit = 0;
+            int lPointer = 0;
+            int RPointer = 1;
+
+            while (RPointer < prices.Length)
+            {
+                int currentPrice = prices[RPointer];
+                int buyPrice = prices[lPointer];
+
+                if (buyPrice < currentPrice)
+                {
+                    int currentProfit = currentPrice - buyPrice;
+                    maxProfit = Math.Max(maxProfit, currentProfit);
+                }
+                else
+                {
+                    lPointer = RPointer;
+                }
+
+                RPointer++;
+            }
+
+            return maxProfit;
+        }
+
+
+    }
+}
