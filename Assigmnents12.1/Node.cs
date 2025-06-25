a79e140c853676d315f3c72e1a2268bee19f0e4e
Filipe Candido Silva Dos Reis <filipe.candidosilvadosreis@outlook.com>
2025-06-25 14:14:47 -0700

    Add files via upload

---
+++ Assigmnents12.1/Node.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asssigments12._1
+{
+    public class Node
+    {
+        public int Data;
+        public Node next;
+        public Node prev;
+        public Node(int data, Node next, Node prev)
+        {
+            this.Data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    class LList
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+        public int Size
+        {
+            get { return this.size; }
+        }
+        public LList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data, null, null);
+            if (IsEmpty())
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                newNode.next = this.head;
+                this.head.prev = newNode;
+                this.head = newNode;
+            }
+            size++;
+        }
+        public bool IsPalindrome()
+        {
+            if (IsEmpty() || this.size == 1) return true;
+            Node left = this.head;
+            Node right = this.tail;
+
+            while (left != right && left.prev != right)
+            {
+                if (left.Data != right.Data)
+                {
+                    return false;
+                }
+
+                left = left.next;
+                right = right.prev;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Assigmnents12.1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Asssigments12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test1s = "a";
+            string test1b = "b";
+            string test2s = "aa";
+            string test2b = "ab";
+            string test3s = "aa";
+            string test3b = "aab";
+            Console.WriteLine(RansomNoteVerification(test1s,test1b));
+            Console.WriteLine(RansomNoteVerification(test2s, test2b));
+            Console.WriteLine(RansomNoteVerification(test3s, test3b));
+
+            LList testTwo = new();
+            testTwo.AddFirst(1);
+            testTwo.AddFirst(2);
+            testTwo.AddFirst(2);
+            testTwo.AddFirst(1);
+
+            LList testTwo2 = new();
+            testTwo2.AddFirst(1);
+            testTwo2.AddFirst(2);
+            testTwo2.AddFirst(3);
+
+            Console.WriteLine(testTwo.IsPalindrome());
+            Console.WriteLine(testTwo2.IsPalindrome());
+
+
+
+
+        }
+        public static bool RansomNoteVerification(string ransomNote, string magazine)//O(n)
+        {
+            Dictionary<char, int> countLetter = new Dictionary<char, int>();//O(n)
+            foreach (char x in magazine)//O(n)
+            {
+                if (countLetter.ContainsKey(x))
+                {
+                    countLetter[x]++;
+                }
+                else
+                {
+                    countLetter[x] = 1;
+                }
+            }
+            foreach (char x in ransomNote)//O(n)
+            {
+                if (!countLetter.ContainsKey(x) || countLetter[x] == 0)
+                {
+                    return false;
+                }
+                countLetter[x]--;
+            }
+            return true;
+        }
+    }
+}
