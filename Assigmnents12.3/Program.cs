3b6802c8c72a712a635da0b8196bd2150f344412
Filipe Candido Silva Dos Reis <filipe.candidosilvadosreis@outlook.com>
2025-06-26 10:34:13 -0700

    Add files via upload

---
+++ Assigmnents12.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Assignment12._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "abbaca";
+            string string2 = "azxxzy";
+            Console.WriteLine(RemoveDuplicates(string1));
+            Console.WriteLine(RemoveDuplicates(string2));
+
+
+        }
+        public static string RemoveDuplicates(string s)//
+        {
+            StringBuilder sb = new();
+
+            foreach (char x in s)
+            {
+                if (sb.Length > 0 && sb[sb.Length - 1] == x)
+                {
+                    sb.Length--;
+                }
+                else
+                {
+                    sb.Append(x);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
